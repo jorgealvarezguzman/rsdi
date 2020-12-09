792a063e1859c0df1d74feddbeb1b4e1a5ff7fe1
cristianpachecob <75406553+cristianpachecob@users.noreply.github.com>
2020-12-09 00:36:35 -0500

    Agregar manejadores: /, /imagen_descargar, /imagen_guardar

--- app.py
+++ app.py
@@ -1,7 +1,40 @@
-from flask import Flask, render_template
+from flask import Flask
+from flask import render_template
+from flask import request
+#from flask import jsonify
 
 app = Flask(__name__)
-
-@app.route("/", methods=["GET", "POST"])
+sesion = True
+@app.route('/')
 def index():
-    return "Esta es la página principal."
+    #sesion = True
+    if sesion == True:
+        return render_template("Dashboard/inicio.html")
+    else:
+        return render_template("Principal/inicio.html")
+
+@app.route('/imagen_descargar')
+def descargar():
+    imagen = request.args.get('imagen')
+    return render_template("Dashboard/descargar.html", imagen=imagen)
+
+@app.route('/imagen_guardar', methods=["POST"])
+def imagen_guardar():
+    if request.method == 'POST':
+        nombre = request.form['nombre']
+        desc = request.form['descripcion']
+        acceso = request.form['acceso']
+        #algoritmo para almacenar la imagen en carpeta destino
+        #algoritmo para almacenar los datos de la imagen en DB
+        #return jsonify(mensaje='Imagen guardada con exito', tipo="ok")
+        redirect("/imagen_crear")
+    else:
+        redirect("/imagen_crear")
+        #return jsonify(mensaje='Lo sentimos, no se puede acceder al recurso', tipo="bad")
+
+
+
+@app.route('/logout')
+def logout():
+    sesion = False
+    
