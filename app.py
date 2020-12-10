352c66f59366d1f1917382cbabcfd9cbbace25b3
cristianpachecob <75406553+cristianpachecob@users.noreply.github.com>
2020-12-10 15:41:20 -0500

    Manejador crear imagen - Cristian Ordoñez

--- app.py
+++ app.py
@@ -111,3 +111,15 @@ def login():
         return render_template("IniciarSesion/index.html")
     except:
         return render_template("IniciarSesion/index.html")
+
+    
+@app.route('/imagen_crear', methods=["POST", "GET"])
+def crearimagen():
+    if request.method == "GET":
+        return render_template("crear3.html")
+    else:
+        return render_template("crear3.html")
+
+@app.route('/imagensubida/', methods=["POST","GET"])
+def imagen():
+    return render_template("imagensubida.html")
